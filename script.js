dee1e9e241ff938f25fbe988150a73a01a727d3f
matthewkolden <matthewkolden2k@gmail.com>
2022-12-06 00:32:04 -0800

    added wpm

--- script.js
+++ script.js
@@ -13,17 +13,21 @@ const startBtn = document.getElementById("start");
 const levelEl = document.getElementById("level");
 let timer;
 let currentQuote;
+let currentQuoteLength;
+let currentQuoteWords;
 let currentStar = 1;
 let currentLevel = 1;
 let progress = 0;
+let wordsTyped = 0;
+let userIndex = 0;
 
 const levels = {
-    1: 60,
-    2: 50,
-    3: 45,
-    4: 40,
-    5: 35,
-    6: 30
+  1: 60,
+  2: 50,
+  3: 45,
+  4: 40,
+  5: 35,
+  6: 30,
 };
 
 // let quote = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
@@ -37,102 +41,135 @@ const levels = {
 //     "Life is what happens when you're busy making other plans."
 // ]
 
-
 // FUNCTIONS
 function startTimer() {
-    if(!timer) {
-        timer = setInterval(() => {
-            timeRemaining--;
-            time.textContent = timeRemaining;
-            if (timeRemaining <= 0) {
-                clearInterval(timer);
-                // LOSE
-            }
-        }, 1000);
-    }
+  if (!timer) {
+    timer = setInterval(() => {
+      timeRemaining--;
+      time.textContent = timeRemaining;
+      if (timeRemaining <= 0) {
+        clearInterval(timer);
+        // LOSE
+      }
+    }, 1000);
+  }
 }
 
 async function getQuote() {
-    typeString.textContent =''; // Resets Quote
-    const response = await fetch("https://api.quotable.io/random");
-    const data = await response.json();
-    if(response.ok) {
-        currentQuote = data.content;
-    }
-    // const randomIndex = Math.floor(Math.random() * 7);
-    // currentQuote = quotes[randomIndex];
-    // console.log(currentQuote)
+  typeString.textContent = ""; // Resets Quote
+  const response = await fetch("https://api.quotable.io/random");
+  const data = await response.json();
+  if (response.ok) {
+    currentQuote = data.content;
+    currentQuoteLength = data.length;
+  }
+  // const randomIndex = Math.floor(Math.random() * 7);
+  // currentQuote = quotes[randomIndex];
+  console.log(currentQuote)
+//   console.log(currentQuoteWords);
+    // countWords();
 }
 
 function typing() {
-    const words = currentQuote.split("");
-    words.forEach((letter) => {
-        const lettersEl = document.createElement("span");
-        lettersEl.classList.add("letter");
-        lettersEl.textContent = letter;
-        typeString.append(lettersEl);
-    })
-    // checkKeys();
+  const letters = currentQuote.split("");
+  letters.forEach((letter) => {
+    const lettersEl = document.createElement("span");
+    lettersEl.classList.add("letter");
+    lettersEl.textContent = letter;
+    typeString.append(lettersEl);
+  });
+
+  // checkKeys();
 }
 
 function checkKeys() {
-    const words = document.querySelectorAll(".letter");
-    let inputValue = userInput.value.split("");
-    for (let i = 0; i < words.length; i++) {
-        const letter = inputValue[i];
-        if(letter === words[i].innerText) {
-            words[i].classList.add("correct");
-            words[i].classList.remove("incorrect");
-        } else {
-            words[i].classList.add("incorrect");
-            words[i].classList.remove("correct");
-        }
+
+  const words = document.querySelectorAll(".letter");
+  let inputValue = userInput.value.split("");
+  for (let i = 0; i < words.length; i++) {
+    const letter = inputValue[i];
+    if (letter === words[i].innerText) {
+      words[i].classList.add("correct");
+      words[i].classList.remove("incorrect");
+    } else {
+      words[i].classList.add("incorrect");
+      words[i].classList.remove("correct");
     }
+  }
 }
 
+function countWords(keyPressed) {
+  const invalid = [".", ",", ";", " "];
+  let numInvalid = 0;
+  let wpm;
+
+  const correctWords = document.querySelectorAll(".correct");
+  for (let i = 0; i < correctWords.length; i++) {
+    userIndex = i;
+  }
+  for (let j = 0; j < invalid.length; j++){
+    if (keyPressed === invalid[j]) {
+      numInvalid++;
+    }
+  }
+  userIndex -= numInvalid;
+
+  wpm = Math.round(((userIndex / 5) / (levels[currentLevel] - timeRemaining)) * 60);
+  document.getElementById("score").textContent = wpm;
+}
+
+
+
 function checkWinner() {
-        const checkCorrect = []
-        for (let i = 0 ; i < document.querySelectorAll(".letter").length; i++) {
-            checkCorrect[i] = document.querySelectorAll(".letter")[i].classList.contains("correct");
-        }
-        if (checkCorrect.includes(false)) {
-            return;
-        } else {
-            userInput.value = "";
-            getQuote();
-            typing();
-            addStar();
-            timeRemaining = levels[currentLevel];
-            startTimer();
-        }
+  const checkCorrect = [];
+
+  for (let i = 0; i < document.querySelectorAll(".letter").length; i++) {
+    checkCorrect[i] = document
+      .querySelectorAll(".letter")
+      [i].classList.contains("correct");
+  }
+
+  // console.log(checkCorrect);
+  if (checkCorrect.includes(false)) {
+    return;
+  } else {
+    userInput.value = "";
+    getQuote();
+    typing();
+    addStar();
+    timeRemaining = levels[currentLevel];
+    startTimer();
+  }
+
+
 }
 
 function addStar() {
-    const currentStarEl = document.querySelectorAll(`.star-${currentStar}`);
-    const stars = document.querySelectorAll(`.star-complete`);
-    if(currentStar < 3) {
-        currentStarEl.forEach((star) => {
-            star.classList.add("star-complete");
-            star.classList.remove("star-incomplete");
-        })
-        progress += (100/3);
-        document.querySelector(".current-progress").style.width = `${progress}%`
-        currentStar++;
-    } else {
-        nextLevel();
-        stars.forEach((star)=> {
-            star.classList.remove("star-complete");
-            star.classList.add("star-incomplete");
-        })
-        progress = 0;
-        document.querySelector(".current-progress").style.width = `${progress}%`
-        currentStar = 1;
-    }
+  const currentStarEl = document.querySelectorAll(`.star-${currentStar}`);
+  const stars = document.querySelectorAll(`.star-complete`);
+  if (currentStar < 3) {
+    currentStarEl.forEach((star) => {
+      star.classList.add("star-complete");
+      star.classList.remove("star-incomplete");
+    });
+    progress += 100 / 3;
+    document.querySelector(".current-progress").style.width = `${progress}%`;
+    currentStar++;
+  } else {
+    nextLevel();
+    stars.forEach((star) => {
+      star.classList.remove("star-complete");
+      star.classList.add("star-incomplete");
+    });
+    progress = 0;
+    document.querySelector(".current-progress").style.width = `${progress}%`;
+    currentStar = 1;
+  }
 }
 
 function nextLevel() {
-    currentLevel++;
-    levelEl.textContent = currentLevel;
+  currentLevel++;
+  levelEl.textContent = currentLevel;
 }
 
 // EVENT LISTENERS
@@ -140,25 +177,26 @@ function nextLevel() {
 document.addEventListener("DOMContentLoaded", getQuote);
 
 startBtn.addEventListener("click", () => {
-    getQuote();
-    if (currentQuote) {
-        startBtn.classList.add("hidden");
-        // on clicking start button timing button starts
-        startTimer();
-        // when typing, user is shown whether what they typed was correct or incorrect
-        typing();
-        // after finishing typing the sentence, score is saved (remaining time)
+  getQuote();
+  if (currentQuote) {
+    startBtn.classList.add("hidden");
+    // on clicking start button timing button starts
+    startTimer();
+    // when typing, user is shown whether what they typed was correct or incorrect
+    typing();
+    // after finishing typing the sentence, score is saved (remaining time)
 
-        // if timer runs out, game is over and asks if user wants to play again
-    }
+    // if timer runs out, game is over and asks if user wants to play again
+  }
 });
 
-userInput.addEventListener("input", () => {
-    if(timer) {
-        checkKeys();
-        // Check to see if user input all words correctly; if so change quote
-        checkWinner();
-    }
-    // console.log(key.data);
+userInput.addEventListener("input", (key) => {
+  if (timer) {
+    checkKeys();
 
-})
+    countWords(key.data);
+
+    // Check to see if user input all words correctly; if so change quote
+    checkWinner();
+  }
+});
